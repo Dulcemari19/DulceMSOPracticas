06559cbcb92d80fb5e95afbffdfa6bc730c5c097
Dulce Maria Sevilla Ortiz <al221910491@gmail.com>
2020-09-28 16:33:02 -0500

    Practica 1 Primer hola mundo , controlador y Ruta

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,8 +81,9 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                  Dulce Maria Sevilla Ortiz Laravel 
                 </div>
+
 
                 <div class="links">
                     <a href="https://laravel.com/docs">Docs</a>
--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('Hellowordl ', function() {
+    return view('hellowordl'); 
+
+
+});
